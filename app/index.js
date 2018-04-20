43fa4513614bdf53c1c602c46e801dba083880a0
Charpell <charpellumeh@gmail.com>
2018-04-20 16:41:35 +0100

    Hide Warning in the IOS Simulator

--- app/index.js
+++ app/index.js
@@ -1,6 +1,9 @@
 console.ignoredYellowBox = ['Remote debugger'];
 
 import React from "react";
+import {YellowBox} from 'react-native';
+YellowBox.ignoreWarnings(['Warning: ReactNative.createElement']);
+
 import { SignedOut, SignedIn } from "./router";
 
 export default class App extends React.Component {
