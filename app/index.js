1c36b11b0e812fc940c3cd10bcade04a4f3d5d97
Charpell <charpellumeh@gmail.com>
2018-04-20 13:52:24 +0100

    Profile/Logout Scren

--- app/index.js
+++ app/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { StyleSheet, Text, View } from "react-native";
 
-import Component from "./screens/SignUp";
+import Component from "./screens/Profile";
 
 export default class App extends React.Component {
   render() {
